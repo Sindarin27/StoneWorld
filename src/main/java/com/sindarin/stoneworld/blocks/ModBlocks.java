11f10bbfbc57de39466d6f8c0df5c1eb429411f1
Sindarin27 <sindarin27@gmail.com>
2020-02-04 14:29:31 +0100

    Add sulfur ore&dust textures, and make sulfur ore a solid block again.

--- src/main/java/com/sindarin/stoneworld/blocks/ModBlocks.java
+++ src/main/java/com/sindarin/stoneworld/blocks/ModBlocks.java
@@ -2,7 +2,6 @@ package com.sindarin.stoneworld.blocks;
 
 import com.sindarin.stoneworld.StoneWorld;
 import net.minecraft.block.Block;
-import net.minecraft.block.FallingBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
@@ -15,14 +14,14 @@ import net.minecraftforge.registries.ObjectHolder;
 @Mod.EventBusSubscriber(modid = StoneWorld.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 @ObjectHolder(StoneWorld.MOD_ID)
 public class ModBlocks {
-    public static final FallingBlock sulfur_ore = null;
+    public static final Block sulfur_ore = null;
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event) {
         //In here you pass in all block instances you want to register.
         //Make sure you always set the registry name.
         event.getRegistry().registerAll(
-                new FallingBlock(Block.Properties.create(Material.SAND, MaterialColor.STONE).hardnessAndResistance(0.6F).harvestLevel(0).harvestTool(ToolType.SHOVEL).sound(SoundType.GROUND)).setRegistryName(StoneWorld.MOD_ID, "sulfur_ore")
+                new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)).setRegistryName(StoneWorld.MOD_ID, "sulfur_ore")
         );
     }
 }
